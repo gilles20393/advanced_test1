ee045a323c42d27aed343ee7566e0c000bbf3a4a
marcdc-tm <marc.decaluwe@thomasmore.be>
2023-02-06 13:25:55 +0100

    3.4

--- src/main/java/be/thomasmore/party/controllers/VenueController.java
+++ src/main/java/be/thomasmore/party/controllers/VenueController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.Optional;
 
@@ -14,12 +15,17 @@ public class VenueController {
     @Autowired
     private VenueRepository venueRepository;
 
-    @GetMapping("/venuedetails")
-    public String venueDetails(Model model) {
-        Optional<Venue> optionalVenue = venueRepository.findById(1);
+    @GetMapping("/venuedetails/{id}")
+    public String venueDetails(Model model, @PathVariable int id) {
+        Optional<Venue> optionalVenue = venueRepository.findById(id);
         if (optionalVenue.isPresent()) {
             model.addAttribute("venue", optionalVenue.get());
         }
         return "venuedetails";
+    }
+
+    @GetMapping("/venuelist")
+    public String venueList() {
+        return "venuelist";
     }
 }
